d2d17bdcd32c3a95eb7951c13910e4377b84eb3d
Julius Kalis <julius.kalis@gmail.com>
2022-05-18 19:36:01 +0200

    fixes organization-chart-settings-options.component.html height bug where the options are not the right height

--- src/app/parts/organization-chart-settings-options/organization-chart-settings-options.component.ts
+++ src/app/parts/organization-chart-settings-options/organization-chart-settings-options.component.ts
@@ -1,4 +1,9 @@
-import {Component, Input, OnDestroy} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 import {OrganizationChartService} from "../../services/organization-chart.service";
 import {Subject, takeUntil} from "rxjs";
 
@@ -7,7 +12,7 @@ import {Subject, takeUntil} from "rxjs";
   templateUrl: './organization-chart-settings-options.component.html',
   styleUrls: ['./organization-chart-settings-options.component.scss']
 })
-export class OrganizationChartSettingsOptionsComponent implements OnDestroy {
+export class OrganizationChartSettingsOptionsComponent implements OnInit, OnDestroy {
 
   @Input() displayMenu: boolean = true;
   @Input() layoutType: string = 'pc';
@@ -23,7 +28,11 @@ export class OrganizationChartSettingsOptionsComponent implements OnDestroy {
   }
 
   ngOnInit() {
-    this.calcHeight(window.innerHeight);
+    if (this.layoutType === 'pc') {
+      this.calcHeight(window.innerHeight, 64);
+    } else {
+      this.calcHeight(window.innerHeight);
+    }
   }
 
   initializeSubscriptions() {
@@ -52,13 +61,15 @@ export class OrganizationChartSettingsOptionsComponent implements OnDestroy {
     this.calcHeight(e.target.innerHeight);
   }
 
-  calcHeight(val: number) {
+  calcHeight(val: number, extraElementHeight: number = 0) {
     const x = document.getElementById('oChartSettingsScrollBox')?.getClientRects()[0];
     if (!x) {return}
+    const elementHeight = x.height + extraElementHeight;
+    console.log('x.height', x.height, 'elementHeight', elementHeight);
     const maxHeight = val - (this.layoutType === 'pc' ? ((36 * 2) + 72) : ((12 * 2) + 72));
 
-    if (maxHeight > x.height) {
-      this.height = x.height;
+    if (maxHeight > elementHeight) {
+      this.height = elementHeight;
       this.setOverflow(true);
     } else {
       this.height = maxHeight;
